00cb19adeb3e22c1c701af5ebf1c3652b1122047
suarez9093 <suarez9093@hotmail.com>
2020-09-22 15:58:21 -0400

    have to build out the db for the replies content. Need to look into possibly creating a new table and have that table reference the users table

--- db/seeds.js
+++ db/seeds.js
@@ -1,3 +1,5 @@
+const { INTEGER } = require("sequelize/types");
+
 CREATE TABLE `users` (
     `id` int(11) NOT NULL AUTO_INCREMENT,
     `message` varchar(255) NOT NULL,
@@ -5,12 +7,22 @@ CREATE TABLE `users` (
     `last_name` varchar(255) NOT NULL,
     `email` varchar(50) NOT NULL,
     `photo` varchar() NOT NULL,
-    // `replies` int(11) DEFAULT '0', => This one I am not too sure of. Might be some sort of join
     PRIMARY KEY (`id`)
   ); 
   
-  
+  // replies needs to map to users. Need to use a foreign key I believe. 
+  CREATE TABLE `replies`(
+    `id` int(11) NOT NULL AUTO_INCREMENT,
+    `message` varchar(255) NOT NULL,
+    `first_name` varchar(255) NOT NULL,
+    `last_name` varchar(255) NOT NULL,
+    `email` varchar(50) NOT NULL,
+    `photo` varchar() NOT NULL,
+    `message_id` INTEGER REFERENCES `users`
+    PRIMARY KEY (`id`)
+  )
   
   INSERT INTO users VALUES 
-  (1,"I just finished reading The Martian and I loved the ending! What do you think?",'Carlos','Diaz',"carlos@email.com",'https://booksloth-profilejpg')
+  (1,"I just finished reading The Martian and I loved the ending! What do you think?",'Carly','Diaz',"carly@email.com",'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80')
+  (2,"I read a book and now I'm so smart!",'Lisa','Brooks',"lisa@email.com",'https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2134&q=80')
   
--- src/components/Cards/Card/index.js
+++ src/components/Cards/Card/index.js
@@ -3,37 +3,46 @@ import "./card.css";
 
 function Card({ message, first_name, last_name, email, photo }) {
   return (
-    <div className="card">
-      <img className="card-img" src={photo} alt="user photo" />
-      <div className="card-main">
-        <p className="card-user">
-          {first_name} {last_name}
-        </p>
-        <p className="card-message">{message}</p>
-        <div className="interact-icons">
-          <div className="interact-icon">
-            <span>
-              <i className="fas fa-share share-icon"></i> 12
-            </span>
-          </div>
-          <div className="interact-icon">
-            <span>
-              <i class="fas fa-heart"></i> 3
-            </span>
-          </div>
-          <div className="interact-icon">
-            <i class="fas fa-share-alt"></i>
+    <>
+      <div className="card">
+        <img className="card-img" src={photo} alt="user photo" />
+        <div className="card-main">
+          <p className="card-user">
+            {first_name} {last_name}
+          </p>
+          <p className="card-message">{message}</p>
+          <div className="interact-icons">
+            <div className="interact-icon">
+              <span>
+                <i className="fas fa-share share-icon"></i> 12
+              </span>
+            </div>
+            <div className="interact-icon">
+              <span>
+                <i class="fas fa-heart"></i> 3
+              </span>
+            </div>
+            <div className="interact-icon">
+              <i class="fas fa-share-alt"></i>
+            </div>
           </div>
         </div>
+        <div className="card-response-container">
+          <button className="card-btn">
+            <span className="card-btn-text">12 min</span>
+            <i class="fas fa-greater-than card-btn-icon"></i>
+          </button>
+          <div className="card-tag">Ask</div>
+        </div>
       </div>
-      <div className="card-response-container">
-        <button className="card-btn">
-          <span className="card-btn-text">12 min</span>
-          <i class="fas fa-greater-than card-btn-icon"></i>
-        </button>
-        <div className="card-tag">Ask</div>
-      </div>
-    </div>
+      {/* <div className="replies">
+        <img src="" alt="" />
+        <div className="card-main">
+          <p className="card-user">Hello</p>
+          <p className="card-message">{message}</p>
+        </div>
+      </div> */}
+    </>
   );
 }
 
