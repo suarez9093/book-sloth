7339813e161525f7505dabb702b7377926cd65fd
suarez9093 <suarez9093@hotmail.com>
2020-09-25 18:03:08 -0400

    updated gif

---
+++ build/precache-manifest.5a124f9968fde999ebd321c44ddf78d3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "450304eaa3506abe2ed05963ee401646",
+    "url": "/book-sloth/index.html"
+  },
+  {
+    "revision": "6c51c4b6a55d49d3b807",
+    "url": "/book-sloth/static/css/main.fbe47b56.chunk.css"
+  },
+  {
+    "revision": "bf63f25276b9c8a4d721",
+    "url": "/book-sloth/static/js/2.cce44f89.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/book-sloth/static/js/2.cce44f89.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c51c4b6a55d49d3b807",
+    "url": "/book-sloth/static/js/main.d3fd518c.chunk.js"
+  },
+  {
+    "revision": "ccad670199ff5b35f77c",
+    "url": "/book-sloth/static/js/runtime-main.67482b7b.js"
+  }
+]);
